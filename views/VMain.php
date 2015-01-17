2b7a7ff508020ac88401d0495a0a7f7169d86f3a
krzychek <krzysiek1993r@gmail.com>
2015-01-17 23:45:44 +0100

    Main list view structure template + css

--- views/VMain.php
+++ views/VMain.php
@@ -6,21 +6,45 @@
         <div class="idea_list">
 <!--            foreach -->
             <div class="idea">
-                <div class="idea_section">
-                    <div class="idea_title"></div>
-                    <div class="idea_tags"></div>
+                <div style="height:0;width:0;clear:both"></div>
+                <div class="idea_main">
+                    <a href="TODO" class="idea_title">First not real, but cute idea</a>
+                    <div class="idea_tags">
+                        <a href="TODO" class="idea_tag">First</a>
+                        <a href="TODO" class="idea_tag">cute</a>
+                    </div>
                 </div>
-                <div class="idea_section">
+                <div class="idea_stats">
                     <div class="idea_stat">
-                        <div class="idea_stat_number"></div>
-                        <div class="idea_stat_description"></div>
+                        <div class="idea_stat_number">0</div>
+                        <div class="idea_stat_desc">votes</div>
                     </div>
                     <div class="idea_stat">
-                        <div class="idea_stat_number"></div>
-                        <div class="idea_stat_description"></div>
+                        <div class="idea_stat_number">0</div>
+                        <div class="idea_stat_desc">comments</div>
                     </div>
                 </div>
             </div>
+            <div class="idea">
+                <div class="idea_main">
+                    <a href="TODO" class="idea_title">Second not real, but awesome idea</a>
+                    <div class="idea_tags">
+                        <a href="TODO" class="idea_tag">second</a>
+                        <a href="TODO" class="idea_tag">cute</a>
+                    </div>
+                </div>
+                <div class="idea_stats">
+                    <div class="idea_stat">
+                        <div class="idea_stat_number">1</div>
+                        <div class="idea_stat_desc">votes</div>
+                    </div>
+                    <div class="idea_stat">
+                        <div class="idea_stat_number">1</div>
+                        <div class="idea_stat_desc">comments</div>
+                    </div>
+                </div>
+            </div>
+            <div style="height:0;width:0;clear:both"></div>
         </div>
     <?php
     }
