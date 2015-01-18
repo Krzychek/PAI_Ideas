a5f820dba88390093e0943206ad8aecf5523000e
krzychek <krzysiek1993r@gmail.com>
2015-01-18 20:13:21 +0100

    check auth before query

--- controllers/Main.php
+++ controllers/Main.php
@@ -6,9 +6,9 @@
 
     function call()
     {
+        Auth::check_auth();
         $conn = MySQL::getConnection();
         $data = $conn->query("SELECT * FROM ideas_main_view")->fetch_all(MYSQLI_ASSOC);
-        Auth::check_auth();
         $view = new VMain($data);
         $view->render();
     }
