50f64f6035910909c28859228b152bd212ac9a3a
krzychek <krzysiek1993r@gmail.com>
2015-01-18 20:49:19 +0100

    change top_panel.php

--- views/top_panel.php
+++ views/top_panel.php
@@ -1,6 +1,6 @@
 <div class="top_panel">
     <ul class="main_menu">
-        <li><a>Panel testowy</a></li>
+        <li><a href="/makeapp/main">MakeApp!</a></li>
         <li style="float: right;"><a class="switch" href="/makeapp/Auth/logout"> wyloguj </a>
             <a class="switch">
                 Zalogowano jako: <?php echo $GLOBALS['user_id']; ?>
