2df43aef308a1d7e4d918ce66b53cb581afbcbda
krzychek <krzysiek1993r@gmail.com>
2015-01-21 21:19:11 +0100

    read nested comments from db

--- controllers/Idea.php
+++ controllers/Idea.php
@@ -6,8 +6,16 @@
         $id = $params[0];
         $conn = MySQL::getConnection();
         $idea_data = $conn->query("SELECT * FROM ideas_details WHERE idea_id = '$id'")->fetch_assoc();
-        $comments_data = $conn->query("SELECT * FROM comments WHERE source_id = '$id'")->fetch_all(MYSQLI_ASSOC);
-        $view = new VIdea($idea_data, $comments_data);
+        $view = new VIdea($idea_data, $this->getComments($id));
         $view->render();
+    }
+
+    private function getComments($id)
+    {
+        $result = MySQL::getConnection()->query("SELECT * FROM comments WHERE source_id = '$id'")->fetch_all(MYSQLI_ASSOC);
+        foreach ($result as &$comment) {
+            $comment['subComments'] = $this->getComments($comment['comment_id']);
+        }
+        return $result;
     }
 }
