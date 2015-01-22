53dcf480b02f8041ddd49611b28dc82e84b41807
krzychek <krzysiek1993r@gmail.com>
2015-01-22 08:10:33 +0100

    order ideas by date

--- controllers/Ideas.php
+++ controllers/Ideas.php
@@ -8,7 +8,7 @@
     {
         Auth::check_auth();
         $conn = MySQL::getConn();
-        $data = $conn->query("SELECT * FROM ideas_overview")->fetch_all(MYSQLI_ASSOC);
+        $data = $conn->query("SELECT * FROM ideas_overview ORDER BY ideas_overview.date ")->fetch_all(MYSQLI_ASSOC);
         $view = new VIdeas($data);
         $view->render();
     }
