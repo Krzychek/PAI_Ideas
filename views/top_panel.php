4a5652439ec04ee884764e188e39289b0720ed0c
krzychek <krzysiek1993r@gmail.com>
2015-01-24 21:44:55 +0100

    logout to new Auth

--- views/top_panel.php
+++ views/top_panel.php
@@ -1,25 +1,10 @@
 <div class="top_panel">
     <ul class="main_menu">
         <li><a href="<?= $GLOBALS['mainFolder'] ?>/ideas">MakeApp!</a></li>
-        <li style="float: right;"><a onclick="logout()" class="switch"> wyloguj </a>
+        <li style="float: right;"><a href="<?= $GLOBALS['mainFolder'] ?>/Auth/logout" class="switch"> wyloguj </a>
             <a class="switch">
                 Zalogowano jako: <?= $GLOBALS['user_id'] ?>
             </a>
         </li>
     </ul>
-    <script>
-        function logout() {
-            var http;
-            if (typeof XMLHttpRequest != 'undefined') http = new XMLHttpRequest();
-            else {
-                try {
-                    http = new ActiveXObject('Msxml2.XMLHTTP');
-                } catch (e) {
-                    http = new ActiveXObject('Microsoft.XMLHTTP');
-                }
-            }
-            http.open("get", '<?= $GLOBALS['mainFolder'] ?>/Auth/dologin/', false, ' ', ' ');
-            http.send("");
-        }
-    </script>
 </div>
