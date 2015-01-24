239fc72a903f3fcdf5f7ae1cee959843f0ca00f1
krzychek <krzysiek1993r@gmail.com>
2015-01-24 19:40:21 +0100

    VLogin updated to new Auth

--- views/VLogin.php
+++ views/VLogin.php
@@ -3,6 +3,19 @@
     function view_body()
     {
         ?>
+        <div class="form">
+            <form onsubmit="return doLogin()">
+                <div class="form_div">
+                    <label>Login: <input type=text id="login-username" class="form_input" name="login"></label>
+                    <label>Hasło: <input type=password id="login-password" class="form_input" name="password"></label>
+                </div>
+                <div class="form_div">
+                    <input type=submit value="ZALOGUJ" class="form_button">
+                </div>
+            </form>
+        </div>
+
+        <script type="application/javascript" src="<?= $GLOBALS['mainFolder'] ?>/scripts/sha1.js"></script>
         <script type="application/javascript">
             function getHTTPObject() {
                 if (typeof XMLHttpRequest != 'undefined') return new XMLHttpRequest();
@@ -17,30 +30,32 @@
                 return false;
             }
             function doLogin() {
-                var http = getHTTPObject();
-                var username = document.getElementById('login-username').value;
-                var password = document.getElementById('login-password').value;
-                http.open("get", '<?= $GLOBALS['mainFolder'] ?>/Auth/dologin/', false, username, password);
-                http.send("");
-                if (http.status == 200) {
-                    document.location = '<?= $GLOBALS['mainFolder'] ?>/Ideas';
-                } else {
-                    alert("Incorrect username and/or password.");
-                }
+                var http = getHTTPObject(),
+                    username = document.getElementById('login-username').value,
+                    password = document.getElementById('login-password').value,
+                    nonce;
+                http.open("GET", '<?= $GLOBALS['mainFolder'] ?>/Auth/getnonce/' + username, true);
+                http.onreadystatechange = function () {
+                    if (http.readyState == 4) {
+                        if (http.status == 200) {
+                            var innerhttp = getHTTPObject();
+                            nonce = http.responseText;
+                            var params = "login=" + username + "&auth=" +
+                                         CryptoJS.SHA1(CryptoJS.SHA1(CryptoJS.SHA1(username) + password) + nonce);
+                            innerhttp.open("POST", '<?= $GLOBALS['mainFolder'] ?>/Auth/dologin/', true);
+                            innerhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                            innerhttp.send(params);
+                        } else {
+                            // TODO
+                        }
+                    }
+                };
+                http.send(null);
+
+
                 return false;
             }
         </script>
-        <div class="form">
-            <form onsubmit="return doLogin()">
-                <div class="form_div">
-                    <label>Login: <input type=text id="login-username" class="form_input" name="login"></label>
-                    <label>Hasło: <input type=password id="login-password" class="form_input" name="password"></label>
-                </div>
-                <div class="form_div">
-                    <input type=submit value="ZALOGUJ" class="form_button">
-                </div>
-            </form>
-        </div>
     <?php
     }
 }
