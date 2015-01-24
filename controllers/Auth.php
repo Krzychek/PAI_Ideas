4f25233c37329576e257a77e601ab11b9352ea8a
krzychek <krzysiek1993r@gmail.com>
2015-01-24 19:38:37 +0100

    fix Auth class

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -22,13 +22,16 @@
         $conn = MySQL::getConn();
         $login = $conn->real_escape_string($_POST['login']);
         $auth = $conn->real_escape_string($_POST['auth']);
-        if (1 != $conn->query("SELECT dologin('$login', '$auth')")->fetch_row()[0]) {
+        if ($cos = $conn->query("SELECT dologin('$login', '$auth')")->fetch_row()[0]) {
             header("Location: " . $GLOBALS['mainFolder'] . "/Auth/");
             die;
         }
         $expire = 9999;
-        $session_id = $conn->query("CALL `get_session_id`('$login', $expire, @p2); SELECT @p2 AS `session_id`;")->fetch_row()[0];
-        setcookie('SESSION_ID', $session_id, time() + $expire * 60);
+
+        $conn->query("CALL `get_session_id`('$login', $expire, @session_id)");
+        $session_id = $conn->query("SELECT @session_id")->fetch_row()[0];
+
+        setcookie('SESSION_ID', $session_id, time() + $expire * 60,$GLOBALS['mainFolder'].'/');
         echo json_encode(array("location" => $GLOBALS['mainFolder'] . "/Ideas/"));
     }
 
@@ -37,7 +40,8 @@
         echo MySQL::getConn()->query("SELECT nonce FROM users WHERE login = '$param[0]'")->fetch_row()[0];
     }
 
-    function logout() {
+    function logout()
+    {
         if (isset($_COOKIE['SESSION_ID'])) {
             $conn = MySQL::getConn();
             $session_id = $conn->real_escape_string($_COOKIE['SESSION_ID']);
@@ -52,3 +56,19 @@
         $view->render();
     }
 }
+
+/*
+BEGIN
+START TRANSACTION;
+
+REPEAT
+	SET @id = MD5(rand());
+UNTIL @id NOT IN(SELECT `session_id` FROM `sessions` FOR UPDATE)
+    END REPEAT;
+
+INSERT INTO `sessions`(`session_id`, `expires`, `login`)
+VALUES(@id, TIMESTAMPADD(MINUTE, minutes, CURRENT_TIMESTAMP), login);
+SELECT @id INTO session_id;
+
+COMMIT;
+END*/
