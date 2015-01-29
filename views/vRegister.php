24fdb9be5915a01c8135a922bca0b4627be384ca
krzychek <krzysiek1993r@gmail.com>
2015-01-29 21:07:05 +0100

    register warnings

--- views/vRegister.php
+++ views/vRegister.php
@@ -2,6 +2,7 @@
 {
     function view_body()
     { ?>
+        <div class="top_panel" style="color: red; text-align: center;" id="warning"></div>
         <form class="form" onsubmit="return postData();">
             <div class="form_div">
                 <label>Login: <input class="form_input" onkeyup="login_check()" type="text" name="login"
@@ -16,6 +17,7 @@
         </form>
         <script type="application/javascript" src="<?= $GLOBALS['mainFolder'] ?>/scripts/sha1.js"></script>
         <script type="text/javascript">
+            var warningDiv = document.getElementById('warning');
             function enableSubmit(params) {
                 if (params instanceof Object) {
                     if ('login' in params) enableSubmit.login = params.login;
@@ -40,46 +42,49 @@
                 var login = document.getElementsByName('login')[0].value;
                 enableSubmit({login: false});
                 if (!login.match(/^[a-zA-Z]+[0-9]*$/)) {
-                    // TODO warning
+                    warningDiv.innerText = 'Login musi zaczynać się literą i składać się wyłacznie z liter lub cyfr';
                     return;
                 }
                 if (login.length < 3) {
-                    // TODO warning
+                    warningDiv.innerText = 'Login musi być dłuższy niż 3 znaki';
                     return;
                 }
                 var xmlhttp = getHTTPObject();
                 xmlhttp.onreadystatechange = function () {
                     if (4 == xmlhttp.readyState) {
-                        if (xmlhttp.responseText == 'available') enableSubmit({login: true});
-                        else enableSubmit({login: false});
+                        if (xmlhttp.responseText == 'available') {
+                            enableSubmit({login: true});
+                            warningDiv.innerText = '';
+                        }
+                        else {
+                            enableSubmit({login: false});
+                            warningDiv.innerText = 'Login zajęty';
+                        }
                     }
                 };
-                xmlhttp.open('GET',
-                    '<?= $GLOBALS['mainFolder'] ?>/Register/check/' + login + '/', true);
+                xmlhttp.open('GET', '<?= $GLOBALS['mainFolder'] ?>/Register/check/' + login + '/', true);
                 xmlhttp.send();
             }
             function pass_check() {
                 enableSubmit({pass: false});
                 var pass = document.getElementsByName('pass')[0].value;
-                if (!pass.match(/(?=.*[0-9]+.*)(?=.*[a-z]+.*)(?=.*[A-Z]+.*)/)) {
-                    // TODO warning
+                if (!pass.match(/(?=.*[0-9]+.*)(?=.*[a-z]+.*)(?=.*[A-Z]+.*)/) || pass < 5) {
+                    warningDiv.innerText = 'Hasło się składać minimum z 5 znaków i posiadać minimum jedną cyfrę, małą i wielką literę';
                     return;
                 }
-                if (pass < 5) {
-                    // TODO warning
-                    return;
-                }
+                warningDiv.innerText = '';
                 if (pass == document.getElementsByName('passr')[0].value) enableSubmit({pass: true});
+                else warningDiv.innerText = 'Hasła nie zgadzają się';
             }
             function postData() {
                 var login = document.getElementsByName('login')[0].value,
                     pass = document.getElementsByName('pass')[0].value,
                     a1 = CryptoJS.SHA1(CryptoJS.SHA1(login) + pass).toString(),
                     http = getHTTPObject(),
-                    params = "login=" + login + "&a1=" + a1;
+                    params = 'login=' + login + '&a1=' + a1;
 
-                http.open("POST", '<?= $GLOBALS['mainFolder'] ?>/Register/post/', true);
-                http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                http.open('POST', '<?= $GLOBALS['mainFolder'] ?>/Register/post/', true);
+                http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
                 http.onreadystatechange = function () {
                     if (http.readyState == 4) {
                         switch (http.status) {
@@ -90,11 +95,8 @@
                                 } catch (e) {
                                 }
                                 break;
-                            case 409:
-                                //TODO username exist
-                                break;
                             default:
-                                alert("Nierozpoznana odpowiedź serwera: " + http.status);
+                                alert('Nierozpoznana odpowiedź serwera: ' + http.status);
                         }
                     }
                 };
